6c9887d620a522a08a1a2b09f7a4ccdfa75e488a
onurasln55 <onurasln55@gmail.com>
2021-02-22 14:57:19 +0300

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,16 @@
+import os
+import fnmatch
+path="h:/" #partition
+key="test"
+for root, dir, files in os.walk(path):#folder walking
+        print (root)
+        print ("")
+        for items in fnmatch.filter(files, "*"):
+                print ("..." + items)
+                name=items.split("."+key,1)
+                #decrypt
+                try:
+                    os.rename(root+"/"+items,root+"/"+name[0])
+                except:
+                    print("bu dosya var")
+        print ("")
