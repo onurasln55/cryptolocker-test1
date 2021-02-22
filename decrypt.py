fe5d435463a35c8253f5e256d09e54889804f10c
onurasln55 <onurasln55@gmail.com>
2021-02-22 15:14:37 +0300

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,13 @@
 import os
 import fnmatch
 path="h:/" #partition
-key="test"
+key=input()
 for root, dir, files in os.walk(path):#folder walking
         print (root)
         print ("")
         for items in fnmatch.filter(files, "*"):
                 print ("..." + items)
                 name=items.split("."+key,1)
-                #decrypt
                 try:
                     os.rename(root+"/"+items,root+"/"+name[0])
                 except:
